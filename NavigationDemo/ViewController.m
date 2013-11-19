d54a0e1666a14948a460287e8fd39b2d505e8e7a
lengqingfeng <lengshengren@lengshengrentekiiMac.local>
2013-11-19 11:11:29 +0800

    Initial Commit

---
+++ NavigationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NavigationDemo
+//
+//  Created by lengshengren on 13-11-19.
+//  Copyright (c) 2013年 lengshengren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
